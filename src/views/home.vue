130c12aa074144e6dafc4bae4b91e4195d5cea95
elvinkyungu <elvinkyungu.75@gmail.com>
2024-04-07 12:32:35 +0200

    ✨ feat: added eslint, prettier, and unocss

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,25 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Menu } from '@ark-ui/vue'
+
+const isOpen = ref(false)
+const value = ref({
+  framework: '',
+  libraries: [],
+})
 </script>
+
 <template>
-  <header>
-    <h1>Hello everyone</h1>
-  </header>
+  <Menu.Root>
+    <Menu.Trigger>Open menu</Menu.Trigger>
+    <Menu.Positioner>
+      <Menu.Content>
+        <Menu.Item id="search">Search</Menu.Item>
+        <Menu.Item id="undo">Undo</Menu.Item>
+        <Menu.Separator />
+        <Menu.Item id="delivery" disabled> Delivery </Menu.Item>
+        <Menu.Item id="unlink">Unlink</Menu.Item>
+      </Menu.Content>
+    </Menu.Positioner>
+  </Menu.Root>
 </template>
--- src/views/notFound.vue
+++ src/views/notFound.vue
@@ -4,7 +4,7 @@
       <div class="absolute bg-gradient-to-r from-[#9adee4] w-1/4 h-1/4 blur-3xl rounded-full z-0 top-1/2 left-1/2"></div>
       <div class="absolute bg-gradient-to-l from-[#bcf9ff] w-1/4 transform rotate-45 h-1/4 blur-3xl rounded-full z-0 top-0 left-0"></div>
       <div class="flex flex-col justify-center text-center z-50">
-        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-black">404</h1>
         <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl ">
           Something's wrong...
         </p>
